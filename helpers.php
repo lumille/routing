68220440f6d94eeddacfecd1c2e9d6a6de6ba48c
Frederic KOLLER <koller.f@gmail.com>
2019-08-19 01:34:59 +0200

    Add redirect function

--- helpers.php
+++ helpers.php
@@ -7,3 +7,16 @@ if (!function_exists('route')) {
         return $app->getRouter()->getUrl($name, $params);
     }
 }
+
+if (!function_exists('redirect'))
+{
+    function redirect($url, $params = [])
+    {
+        if (!preg_match('#^https?:\/\/#', $url)) {
+            $url = route($url, $params) ?? $url;
+        }
+
+        header('Location: ' . $url);
+        exit;
+    }
+}
